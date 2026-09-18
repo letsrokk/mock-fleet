ba131f2cc4720467dd23de95023d3d3db4d619e8
letsrokk <mayer.dmitry@gmail.com>
2026-09-18 19:15:11 +0200

    145: fix(api): report application version in OpenAPI

--- fleet-api/src/main/java/com/github/letsrokk/FleetApplication.java
+++ fleet-api/src/main/java/com/github/letsrokk/FleetApplication.java
@@ -1,9 +1,6 @@
 package com.github.letsrokk;
 
 import jakarta.ws.rs.core.Application;
-import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
-import org.eclipse.microprofile.openapi.annotations.info.Info;
 
-@OpenAPIDefinition(info = @Info(title = "Mock Fleet API", version = "2.0.0"))
 public class FleetApplication extends Application {
 }
